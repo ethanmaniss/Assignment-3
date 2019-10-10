b9d4006631be24bc2aafa9deb1ac230fe3c51a3d
ethanmaniss <maniss@chapman.edu>
2019-10-10 17:45:04 +0000

    first commit

---
+++ GenStack.cpp
@@ -0,0 +1,90 @@
+#include "GenStack.h"
+#include <iostream>
+
+using namespace std;
+// default constructor
+GenStack::GenStack()
+{
+    // initialization of default values
+    myArray = new char[128];
+    size = 128;
+    top = -1; // will eventually be preincremented to equal index value of 0
+}
+
+GenStack::GenStack(int maxSize) // overloaded constructor
+{
+    myArray = new char[maxSize];
+    size = maxSize;
+    top = -1;
+}
+
+GenStack::~GenStack()
+{
+    // lets build some character
+    // your job to research this
+    // (one line of code to delete array)
+    delete[] myArray;
+}
+
+void GenStack::push(char d)
+{
+    // need to check error/boundary check
+    // this is your job
+    if(isFull())
+    {
+        cout << "Making a larger array." << endl;
+        char* newArr = new char[2*size];
+        for(int i = 0; i < size; ++i)
+        {
+            newArr[i] = myArray[i];
+        }
+        size *= 2;
+        delete myArray;
+        myArray = newArr;
+    }
+    else
+    {
+        myArray[++top] = d;
+    }
+}
+
+char GenStack::pop()
+{
+    // error checking, make sure it's not isEmpty
+    if(isEmpty())
+    {
+        cout << "Array is empty." << endl;
+    }
+    else
+    {
+        return myArray[top--];
+    }
+}
+
+char GenStack::peek()
+{
+    // check if it's empty
+    if(isEmpty())
+    {
+        cout << "Array is empty." << endl;
+    }
+    else
+    {
+        return myArray[top];
+    }
+}
+
+bool GenStack::isFull() // will return either true or false
+{
+    return (top == size - 1);
+}
+
+bool GenStack::isEmpty()
+{
+    return (top == -1);
+}
+
+int GenStack::getSize()
+{
+    return size;
+}
---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include "GenStack.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    GenStack myStack(12);
+
+    cout << myStack.getSize() << endl;
+
+    myStack.push('S');
+    myStack.push('S');
+    myStack.push('I');
+    myStack.push('N');
+    myStack.push('A');
+    myStack.push('M');
+    myStack.push(' ');
+    myStack.push('N');
+    myStack.push('A');
+    myStack.push('H');
+    myStack.push('T');
+    myStack.push('E');
+    myStack.push(' ');
+    myStack.push('W');
+    myStack.push('W');
+    myStack.push('W');
+
+    cout << myStack.getSize() << endl;
+
+
+    while(!myStack.isEmpty())
+    {
+        cout << myStack.pop() << endl;
+    }
+
+    cout << myStack.pop() << endl;
+
+    char userChoice = 'y';
+
+    while(userChoice == 'y' || userChoice == 'Y')
+    {
+        
+
+        cout << "Would you like to analyze another file?" << endl;
+        cout << "Enter 'y' for yes or any other character for no." << endl;
+        cin >> userChoice;
+    }
+
+    return 0;
+}
